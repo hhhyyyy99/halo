ff866e82acf7287308ffd419d264c681a127f19b
dw-json <1025942836@qq.com>
2024-06-20 08:57:21 +0800

    fix: 修复runScript方法

--- packages/pluginManager/src/index.ts
+++ packages/pluginManager/src/index.ts
@@ -1,8 +1,11 @@
 import {PluginManager} from './Core'
 import AbstractPlugin from "./AbstractPlugin";
 import PluginEventEmitter from "./PluginEventEmitter";
+import {ScriptType}from './interface';
+import {Plugin, PluginDefine, PluginLifecycle} from './Plugin';
 import ProxySandBox from "./Sandbox";
-export type { Plugin, PluginDefine, PluginLifecycle } from "./Plugin";
-export { CoreConfig } from "./Core";
-export { AbstractPlugin, PluginEventEmitter, ProxySandBox }
+import { CoreConfig } from "./Core";
+
+export type { Plugin, PluginDefine, PluginLifecycle }
+export { AbstractPlugin, PluginEventEmitter, ProxySandBox,ScriptType,CoreConfig }
 export default PluginManager
